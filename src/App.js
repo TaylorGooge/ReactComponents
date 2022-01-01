d6caadc99e3d9bc70d2ac909cb1199a8725f216e
Taylor Googe <taylorgooge@Taylors-MacBook-Pro-2.local>
2022-01-01 17:40:38 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter as BrowserRouter, Route , Routes} from "react-router-dom";
 import { HomePage } from './Components/Pages/HomePage'
 import { AboutPage } from "./Components/Pages/AboutPage"
+import { ContactPage } from "./Components/Pages/ContactPage"
+import { PortfolioPage } from "./Components/Pages/PortfolioPage"
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Routes>
             <Route path='/'> < HomePage /> </Route>
             <Route path='/about'> < AboutPage /> </Route>
+            <Route path='/contact'> < ContactPage /> </Route>
+            <Route path='/portfolio'> < PortfolioPage /> </Route>
           </Routes>
         </BrowserRouter>
       
